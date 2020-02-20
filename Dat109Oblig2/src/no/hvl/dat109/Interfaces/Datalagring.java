677e3852af8d3a1ee31f8576cbc2e06cab01e891
borgar <borgar@192.168.80.200>
2020-02-20 07:19:17 +0100

    datalagring edit

--- Dat109Oblig2/src/no/hvl/dat109/Interfaces/Datalagring.java
+++ Dat109Oblig2/src/no/hvl/dat109/Interfaces/Datalagring.java
@@ -3,19 +3,21 @@ package no.hvl.dat109.Interfaces;
 import java.sql.Timestamp;
 import java.util.List;
 
-import no.hvl.dat109.Entity.Adress;
 import no.hvl.dat109.Entity.BilDB;
 import no.hvl.dat109.Entity.Utleigekontor;
 
 public interface Datalagring {
 
-		public void lagreBil(Bil bil);
-		public Bil hentBil(String regnr);
-		public List<BilDB> finnledigeBilar(Timestamp fra, Timestamp til, Utleigekontor fraLokasjon);
-		
+	public void lagreBil(Bil bil);
+
+	public Bil hentBil(String regnr);
+
+	public List<BilDB> finnledigeBilar(Timestamp fra, Timestamp til, Utleigekontor fraLokasjon);
+
 	public int lagreAdresse(AdressInterface adress);
+
 	public void lagreNyKunde(KundeInterface kunde);
+
 	public Utleigekontor hentUtleigekontor(int id);
-	
-		
+
 }
